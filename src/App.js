b33cbf21963ddad7a98f6e645dedacf92ffcd552
Roman Basharin <roman.basharin@quantori.com>
2021-05-20 21:41:01 +0300

    Basic project config

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Body from './components/Body'
+
+const Header = props => (
+  <div>Header</div>
+)
+
+const Footer = props => (
+  <div>Footer</div>
+)
+
+const App = () => (
+  <div>
+    <Header />
+    <Body />
+    <Footer />
+  </div>
+)
+
+export default App

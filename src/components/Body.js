d56e2860ec80c5bc982e7ce58081e21267821cf4
Roman Basharin <roman.basharin@quantori.com>
2021-05-20 23:50:37 +0300

    Create From component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Clock from './Clock'
 import Users from './Users'
+import Form from './Form'
 
 const Body = () => (
   <div>
     <Clock />
     <Users />
+    <Form />
   </div>
 )
 
---
+++ src/components/Form.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+class Form extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			firstName: 'Roman',
+			text: 'Est duis magna do nisi et labore magna cupidatat cillum laborum.',
+			fruit: 'coconut',
+		}
+	}
+
+	handleChange = event => {
+		this.setState({ [event.target.name]: event.target.value })
+	}
+
+	render() {
+		return (
+			<div style={{ margin: '20px 0' }}>
+				<div>Ваше имя:  {this.state.firstName || 'не указано'}</div>
+				<input
+					name="firstName"
+					type="text"
+					value={this.state.firstName}
+					onInput={this.handleChange}
+				/>
+
+				<div style={{ marginTop: '10px' }}>Ваш текст:  {this.state.text || 'не указан'}</div>
+				<textarea
+					name="text"
+					value={this.state.text}
+					onInput={this.handleChange}
+				/>
+
+				<div style={{ marginTop: '10px' }}>Ваш фрукт:  {this.state.fruit || 'не указан'}</div>
+				<select
+					name="fruit"
+					value={this.state.fruit}
+					onChange={this.handleChange}
+				>
+					<option value="grapefruit">Грейпфрут</option>
+					<option value="lime">Лайм</option>
+					<option value="coconut">Кокос</option>
+					<option value="mango">Манго</option>
+				</select>
+			</div>
+		)
+	}
+}
+
+export default Form

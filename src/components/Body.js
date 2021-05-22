09f9b5466ea168a45a4b5d055d49f83d53440011
Roman Basharin <roman.basharin@quantori.com>
2021-05-22 15:35:12 +0300

    feat Calculator component

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Clock from './Clock'
 import Users from './Users'
 import Form from './Form'
+import Calculator from './Calculator'
 
 const Body = () => (
   <main>
     <Clock />
     <Users />
     <Form />
+    <Calculator />
   </main>
 )
 

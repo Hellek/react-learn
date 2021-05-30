3ee05088f5ccf878d931ad1e4a6ae4f09608771b
Roman Basharin <roman.basharin@quantori.com>
2021-05-30 19:52:01 +0300

    feat: add email

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,5 +1,8 @@
 const Contacts = () => (
-  <div>Это страница контактов</div>
+  <div>
+    <span>Get in touch with </span>
+    <a href="mailto:my@hellek.ru">my@hellek.ru</a>
+  </div>
 )
 
 export default Contacts

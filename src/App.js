3f1a638337743ed6b0fd091d57472de924e79d9b
Roman Basharin <roman.basharin@quantori.com>
2021-05-31 01:10:04 +0300

    feat: pages js -> tsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { HashRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Common from './pages/Common'
-import Contacts from './pages/Contacts'
-import Mobx from './pages/Mobx'
+import Home from './pages/Home.jsx'
+import Common from './pages/Common.jsx'
+import Contacts from './pages/Contacts.jsx'
+import Mobx from './pages/Mobx.jsx'
 import Header from './components/Header'
 import Footer from './components/Footer'
 

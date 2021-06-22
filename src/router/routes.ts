93bfb41f780d43524b2a5366845406e6c1d8c5bd
Roman Basharin <roman.basharin@quantori.com>
2021-06-22 13:32:02 +0300

    hide algo page

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,5 +1,5 @@
 import Achievements from '@pages/Achievements'
-import Algorithms from '@pages/Algorithms'
+// import Algorithms from '@pages/Algorithms'
 import Common from '@pages/Common'
 import Contacts from '@pages/Contacts'
 import Home from '@pages/Home'
@@ -16,11 +16,11 @@ export const routes = [
 		component: Achievements,
 		label: 'Achievements',
 	},
-	{
+	/* {
 		path: '/algorithms',
 		component: Algorithms,
 		label: 'Algorithms',
-	},
+	}, */
 	{
 		path: '/common',
 		component: Common,

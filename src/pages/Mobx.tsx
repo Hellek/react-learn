bea7abb8445e718f5091da2e68b15835dab7f8a3
Roman Basharin <roman.basharin@quantori.com>
2021-08-03 18:21:16 +0300

    set custom tailwind colors

--- src/pages/Mobx.tsx
+++ src/pages/Mobx.tsx
@@ -2,22 +2,22 @@ import { makeAutoObservable } from 'mobx'
 import { observer } from 'mobx-react-lite'
 
 class LocalStore {
-  value = 0
-  timesClicked = 0
+	value = 0
+	timesClicked = 0
 
-  constructor() {
-  	makeAutoObservable(this)
-  }
+	constructor() {
+		makeAutoObservable(this)
+	}
 
-  increase() {
-  	this.value++
-  	this.timesClicked++
-  }
+	increase() {
+		this.value++
+		this.timesClicked++
+	}
 
-  decrease() {
-  	this.value--
-  	this.timesClicked++
-  }
+	decrease() {
+		this.value--
+		this.timesClicked++
+	}
 }
 
 const StoreInstance = new LocalStore()
@@ -25,6 +25,7 @@ const StoreInstance = new LocalStore()
 export const MobxPage = observer(() => {
 	return (
 		<div>
+			<code className="mb-1 inline-block">mobx-react-lite</code>
 			<div className="mb-2">Times clicked: {StoreInstance.timesClicked}</div>
 			<div className="mb-2">Balance: {StoreInstance.value}</div>
 			<button className="mb-2 mr-2" onClick={() => { StoreInstance.increase() }}>Increase</button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		colors: {
+			transparent: colors.transparent,
+			white: colors.white,
+			blue: {
+				400: colors.blue['400'],
+				500: colors.blue['500'],
+			},
+			gray: {
+				600: colors.gray['600'],
+				800: colors.gray['800'],
+				900: colors.gray['900'],
+				code: '#1a202c',
+			},
+			green: {
+				code: '#48bb78',
+				'code-light': '#0c344b',
+			},
+		},
 		extend: {
 			fontSize: {
 				10: '10px',

8c9b5fe5d14a61263c72b1b69cce09611000a845
Roman Basharin <roman.basharin@quantori.com>
2021-08-08 19:24:42 +0300

    add example of redux simple using

---
+++ src/components/redux/Simple.tsx
@@ -0,0 +1,61 @@
+import { FC } from 'react'
+import { Provider, useSelector } from 'react-redux'
+import { createStore } from 'redux'
+
+interface ReduxAction {
+	type: string;
+	payload?: any;
+}
+
+interface State {
+	timesClicked: number
+	value: number
+}
+
+const defaultState = {
+	timesClicked: 0,
+	value: 0,
+}
+
+const counterReducer = function (state: State = defaultState, action: ReduxAction) {
+	switch (action.type) {
+	case 'counter/increase':
+		return { value: state.value + 1, timesClicked: state.timesClicked + 1 }
+	case 'counter/decrease':
+		return { value: state.value - 1, timesClicked: state.timesClicked + 1 }
+	default:
+		return state
+	}
+}
+
+
+// Create a Redux store holding the state of your app.
+// Its API is { subscribe, dispatch, getState }.
+const store = createStore(counterReducer)
+
+store.subscribe(() => console.info(store.getState()))
+
+const CounterComponent: FC = () => {
+	const timesClicked = useSelector((state: State) => state.timesClicked)
+	const value = useSelector((state: State) => state.value)
+
+	return (
+		<div>
+			<code className="mb-1 inline-block">react-redux</code>
+			<div className="mb-2">Times clicked: {timesClicked}</div>
+			<div className="mb-2">Balance: {value}</div>
+			<button className="mb-2 mr-2" onClick={() => { store.dispatch({ type: 'counter/increase' }) }}>Increase</button>
+			<button className="mb-2" onClick={() => { store.dispatch({ type: 'counter/decrease' }) }}>Decrease</button>
+		</div>
+	)
+}
+
+export const ReduxSimple: FC = () => {
+	return (
+		<Provider store={store}>
+			<CounterComponent />
+		</Provider>
+	)
+}
+
+export default ReduxSimple
--- src/pages/Redux.tsx
+++ src/pages/Redux.tsx
@@ -1,8 +1,11 @@
 import { FC } from 'react'
 
+import ReduxSimple from '@components/redux/Simple'
+
 export const ReduxPage: FC = () => {
 	return (
 		<div className="flex">
+			<ReduxSimple />
 			<div className="mr-5" />
 		</div>
 	)

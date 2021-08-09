579f82c16bb9e4c4da7541230c81c04b6d407dae
Roman Basharin <roman.basharin@quantori.com>
2021-08-09 15:10:05 +0300

    add classnames to SimpleCard

--- src/ui/SimpleCard.tsx
+++ src/ui/SimpleCard.tsx
@@ -1,11 +1,13 @@
 import { ReactElement, ReactNode } from 'react'
+import cn, { Argument } from 'classnames'
 
 interface CardProps {
 	children?: ReactNode
+	className?: Argument
 }
 
-export const SimpleCard = ({ children }: CardProps): ReactElement => (
-	<div className="p-3 mb-4 rounded-lg border-2 border-blue-400">
+export const SimpleCard = ({ children, className }: CardProps): ReactElement => (
+	<div className={cn('p-3 mb-4 rounded-lg border-2 border-blue-400', className)}>
 		{children}
 	</div>
 )

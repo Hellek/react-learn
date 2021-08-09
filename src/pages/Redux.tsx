b3089f4a9cac59fd99de6934b3ca77bdf30501e3
Roman Basharin <roman.basharin@quantori.com>
2021-08-09 15:10:35 +0300

    add SimpleCard wrapper to Redux examples

--- src/pages/Redux.tsx
+++ src/pages/Redux.tsx
@@ -1,14 +1,19 @@
 import { FC } from 'react'
 
+import { SimpleCard } from '@ui/SimpleCard'
 import ReduxToolkit from '@components/redux/ReduxToolkit'
 import ReduxSimple from '@components/redux/Simple'
 
 export const ReduxPage: FC = () => {
 	return (
 		<div className="flex">
-			<ReduxSimple />
-			<div className="mr-5" />
-			<ReduxToolkit />
+			<SimpleCard className="mr-4">
+				<ReduxSimple />
+			</SimpleCard>
+
+			<SimpleCard>
+				<ReduxToolkit />
+			</SimpleCard>
 		</div>
 	)
 }

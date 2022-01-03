9b7f1c088e9e039ad0a8b7d8b1950539bb33e4d2
Roman Basharin <roman.basharin@quantori.com>
2022-01-03 19:09:59 +0300

    Add webpack progress bar plugin

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const CracoAlias = require('craco-alias')
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
 
 module.exports = {
 	plugins: [
@@ -14,6 +15,13 @@ module.exports = {
 	style: {
 		postcss: {
 			plugins: [require('tailwindcss'), require('autoprefixer')],
+		},
+	},
+	webpack: {
+		plugins: {
+			add: [
+				ProgressBarPlugin(),
+			],
 		},
 	},
 }
